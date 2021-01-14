84ba8f189cd9d4a07d1eb6f7719efa4d5107232b
Jun Wu <quark@lihdd.net>
2021-01-13 18:32:48 -0800

    backend: support Python URLs

--- backend/notebackend/src/backend/url.rs
+++ backend/notebackend/src/backend/url.rs
@@ -1,23 +1,30 @@
+use super::blob;
+use super::dylib;
 use super::Id;
 use super::TreeBackend;
 use std::io;
-use std::io::Result;
+use std::{io::Result, path::Path};
 
 /// Load a backend from a URL.
 pub fn open(url: &str) -> Result<Box<dyn TreeBackend<Id = Id>>> {
-    use super::blob;
-    use std::path::Path;
-
-    let foonote = "foonote:";
-    let backend: Box<dyn TreeBackend<Id = Id>> = if url.starts_with(foonote) {
-        let path = Path::new(&url[foonote.len()..]);
-        Box::new(blob::SingleFileBackend::from_path(path)?.with_trash(true))
-    } else {
-        return Err(io::Error::new(
-            io::ErrorKind::InvalidInput,
-            format!("invalid backend URL: {}", url),
-        ));
+    let (scheme, path) = {
+        let mut split = url.splitn(2, ':');
+        let scheme = split.next().unwrap_or_default();
+        let path = split.next().unwrap_or_default();
+        (scheme, path)
     };
 
-    Ok(backend)
+    match scheme {
+        "foonote" => Ok(Box::new(
+            blob::SingleFileBackend::from_path(&Path::new(path))?.with_trash(true),
+        )),
+        "python" | "python-base64" => Ok(Box::new(dylib::DylibBackend::open(
+            "notebackend_python",
+            url,
+        )?)),
+        _ => Err(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            format!("invalid backend URL: {}", url),
+        )),
+    }
 }
