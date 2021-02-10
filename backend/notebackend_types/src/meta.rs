0226f5043d45a8ee1322f18433aaf73d4c060920
Jun Wu <quark@lihdd.net>
2021-02-09 23:15:30 -0800

    backend: comment about is_copyable

--- backend/notebackend_types/src/meta.rs
+++ backend/notebackend_types/src/meta.rs
@@ -1,6 +1,8 @@
 use crate::TreeBackend;
 use std::io::Result;
 pub trait TreeMeta: TreeBackend {
+    /// Copyable: Whether "copy" can be used to copy the node.
+    /// For example, special node "Trash" is not copyable.
     fn is_copyable(&self, id: Self::Id) -> Result<bool> {
         Ok(self.extract_meta(id, "copyable=")? != "false")
     }
