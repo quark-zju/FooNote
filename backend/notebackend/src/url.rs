33f17cc49d43c2b01b5d1bf42c49dc47d90c5d91
Jun Wu <quark@lihdd.net>
2021-02-09 23:15:17 -0800

    backend: simplify url handling

--- backend/notebackend/src/url.rs
+++ backend/notebackend/src/url.rs
@@ -7,38 +7,21 @@ use std::{io::Result, path::Path};
 /// Load a backend from a URL.
 pub fn open(url: &str) -> Result<Box<dyn TreeBackend<Id = Id>>> {
     log::info!("open url: {}", url);
-    let (scheme, path) = {
-        let mut split = url.splitn(2, ':');
-        let segment1 = split.next().unwrap_or_default();
-        let segment2 = split.next().unwrap_or_default();
-        if segment2.is_empty() {
-            ("", url)
-        } else {
-            (segment1, segment2)
-        }
-    };
 
-    match scheme {
-        "git" => Ok(Box::new(
-            backend::GitBackend::from_git_url(url, None)?.with_trash(true),
-        )),
-        "foonote" => Ok(Box::new(
-            backend::SingleFileBackend::from_path(&Path::new(path))?.with_trash(true),
-        )),
-        "python" | "python-base64" => Ok(Box::new(backend::DylibBackend::open(
-            "notebackend_python",
-            url,
-        )?)),
-        "memory" => Ok(Box::new(backend::MemBackend::empty())),
-        _ if url.ends_with(".git") => Ok(Box::new(
-            backend::GitBackend::from_git_url(url, None)?.with_trash(true),
-        )),
-        "" => Ok(Box::new(
-            backend::SingleFileBackend::from_path(&Path::new(path))?.with_trash(true),
-        )),
-        _ => Err(io::Error::new(
+    let b: Box<dyn TreeBackend<Id = Id>> = if url.ends_with(".git") || url.ends_with(".git/") {
+        Box::new(backend::GitBackend::from_git_url(url, None)?.with_trash(true))
+    } else if url.ends_with(".foonote") {
+        Box::new(backend::SingleFileBackend::from_path(&Path::new(url))?.with_trash(true))
+    } else if url == "memory" {
+        Box::new(backend::GitBackend::from_git_url(url, None)?.with_trash(true))
+    } else if url.ends_with(".py") {
+        Box::new(backend::DylibBackend::open("notebackend_python", url)?)
+    } else {
+        return Err(io::Error::new(
             io::ErrorKind::InvalidInput,
             format!("invalid backend URL: {}", url),
-        )),
-    }
+        ));
+    };
+
+    Ok(b)
 }
