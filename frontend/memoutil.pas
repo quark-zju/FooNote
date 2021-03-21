d57e7a7e9f366147f076cbb8999b22e25ca17fd8
Jun Wu <quark@lihdd.net>
2021-03-20 21:46:52 -0700

    Revert "f/memo: drop smart tab feature for TMemo"
    This reverts commit cbb860b030422de50105c25bfbde3a0e713ee9f6.
    
    The tab indentation feature can be useful on macOS. It seems the macOS
    TMemo is pretty feature-rich (ex. spell check, look up dictionary, text
    to speech, multi-undo, etc) and there is no official doc about how to
    use Scintilla on macOS.

--- frontend/memoutil.pas
+++ frontend/memoutil.pas
@@ -8,21 +8,88 @@ uses
   Classes, SysUtils, StdCtrls, LazUTF8;
 
 
+// Find the range of Y covering selected text.
+procedure SelectedYRange(Memo: TMemo; var YStart: longint; var YEnd: longint);
+
+// Select full lines in a range.
+procedure SelectLines(Memo: TMemo; YStart: longint; YEnd: longint);
+
+// Dedent a line.
+procedure DedentLine(Memo: TMemo; Y: longint);
+
+// Indent a line.
+procedure IndentLine(Memo: TMemo; Y: longint);
+
 // Handle TAB, ENTER in a smarter way.
 procedure SmartKeyDown(Memo: TMemo; var Key: word; Shift: TShiftState);
 
 implementation
 
+procedure DedentLine(Memo: TMemo; Y: longint);
+var
+  Line: string;
+begin
+  Line := Memo.Lines[Y];
+  if Line.StartsWith('  ') then begin
+    Line := Line.Substring(2);
+    Memo.Lines[Y] := Line;
+  end;
+end;
+
+procedure IndentLine(Memo: TMemo; Y: longint);
+var
+  Line: string;
+begin
+  Line := Memo.Lines[Y];
+  if not Line.IsEmpty then begin
+    Line := Line.Insert(0, '  ');
+    Memo.Lines[Y] := Line;
+  end;
+end;
+
+
+// Find the range of Y covering selected text.
+procedure SelectedYRange(Memo: TMemo; var YStart: longint; var YEnd: longint);
+var
+  Text: string;
+begin
+  if Memo.SelLength = 0 then begin
+    YStart := Memo.CaretPos.Y;
+    YEnd := YStart;
+  end;
+  // Is there a faster way? Memo.Lines? Is it separated as CRLF or LF?
+  Text := Memo.Text;
+  YStart := LazUtf8.UTF8LeftStr(Text, Memo.SelStart).CountChar(#10);
+  YEnd := YStart + Memo.SelText.CountChar(#10);
+end;
+
+procedure SelectLines(Memo: TMemo; YStart: longint; YEnd: longint);
+var
+  I, Len, EolLen: longint;
+begin
+  Memo.CaretPos := TPoint.Create(0, YStart);
+  Len := 0;
+  EolLen := LazUtf8.UTF8Length(Memo.Lines.LineBreak);
+  for I := YStart to YEnd do begin
+    Len += LazUtf8.UTF8Length(Memo.Lines[I]);
+    if I < YEnd then Len += EolLen;
+  end;
+  Memo.SelLength := Len;
+end;
+
 procedure SmartKeyDown(Memo: TMemo; var Key: word; Shift: TShiftState);
 var
-  Y: longint;
+  X, Y, YStart, YEnd: longint;
   Line: string;
   SpaceCount: integer;
   Prefix: string;
 begin
   // X, Y, SelLen: UTF8 Code Points.
   // SelLen uses CRLF on Windows as EOL.
+  X := Memo.CaretPos.X;
   Y := Memo.CaretPos.Y;
+  YStart := 0;
+  YEnd := 0;
 
   // Auto indent: try to insert spaces from the previous line.
   // "  - foo <ENTER>" will insert "  " in the next line.
@@ -43,6 +110,27 @@ begin
         key := 0;
       end;
     end;
+  end;
+
+  // Smart TAB: Indent or dedent selected lines.
+  if key = 9 then begin
+    if ssShift in Shift then begin
+      // De-dent selected lines.
+      SelectedYRange(Memo, YStart, YEnd);
+      for Y := YStart to YEnd do DedentLine(Memo, Y);
+      SelectLines(Memo, YStart, YEnd);
+    end else begin
+      if Memo.SelLength = 0 then begin
+        // No selection. Insert 2 spaces.
+        Memo.SelText:='  ';
+      end else begin
+        // With selection. Indent lines.
+        SelectedYRange(Memo, YStart, YEnd);
+        for Y := YStart to YEnd do IndentLine(Memo, Y);
+        SelectLines(Memo, YStart, YEnd);
+      end;
+    end;
+    key := 0;
   end;
 end;
 
