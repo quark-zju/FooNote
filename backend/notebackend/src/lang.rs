7500f3d9f58e9a42f6d4261f6bfe28b19de4801f
Jun Wu <quark@lihdd.net>
2021-03-28 13:10:25 -0700

    Revert "b/lang: figure out system default locale from Rust"
    It adds too much bloat in the DLL. (1.5 -> 2.6MB)
    
    This reverts commit a9439f4080158fef2f224c6a8daa1a7762c536fd.

--- backend/notebackend/src/lang.rs
+++ backend/notebackend/src/lang.rs
@@ -1,6 +1,5 @@
 //! i18n utilities
 
-use notebackend_types::log;
 use std::fmt;
 /// Describe a language id.
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
@@ -11,21 +10,6 @@ pub enum LangId {
 }
 
 pub static mut CURRENT_LANG_ID: LangId = LangId::En;
-
-impl LangId {
-    /// Get language Id from system locale.
-    pub fn from_system() -> Self {
-        let loc = locale_config::Locale::user_default();
-        let loc_str = loc.to_string().to_lowercase();
-        let lang = if loc_str.contains("cn") || loc_str.starts_with("zh") {
-            LangId::Cn
-        } else {
-            LangId::En
-        };
-        log::info!("locale: {} => {}", &loc, &lang);
-        lang
-    }
-}
 
 #[macro_export]
 macro_rules! t {
@@ -51,15 +35,5 @@ impl fmt::Display for LangId {
             LangId::En => "en",
         };
         f.write_str(name)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_lang_from_system() {
-        let _ = LangId::from_system();
     }
 }
